4e177d12b4aa11f7bb3cd37ea0e88ef6ca72d51a
Lucus Ra <72078332+lucusra@users.noreply.github.com>
2021-02-17 14:27:01 +1100

    Update FlareToken.sol

--- contracts/FlareToken.sol
+++ contracts/FlareToken.sol
@@ -85,8 +85,6 @@ abstract contract FlareToken is ERC20 {
     uint8 private _decimals = 18;                       
     uint256 private _totalSupply;                                                                                    
 
-    mapping(address => mapping(address => uint256)) _allowanceOf;
-
     address payable owner;
 
     struct Checkpoint {
@@ -96,6 +94,7 @@ abstract contract FlareToken is ERC20 {
 
     Checkpoint[] totalSupplyHistory;
     mapping (address => Checkpoint[]) balances;
+    mapping(address => mapping(address => Checkpoint[])) allowances;
 
     constructor() {    
         _totalSupply = 1000000 * (18**10);   // 1 mil tokens
@@ -107,32 +106,41 @@ abstract contract FlareToken is ERC20 {
     }
 
     function balanceOf(address tokenOwner) override external view returns (uint balance) {
-        return balances[tokenOwner][(balances[tokenOwner].length)].value;
+        return balances[tokenOwner][balances[tokenOwner].length].value;
     }
 
     function viewAllowance(address tokenOwner, address spender) override external view returns (uint remaining) {
-        return _allowanceOf[tokenOwner][spender];
+        return allowances[spender][tokenOwner][allowances[spender][tokenOwner].length].value;
     }
 
     function transfer(address _to, uint _amount) override external returns (bool success) {
-        require(balances[msg.sender][(balances[msg.sender].length)].value != 0, "ERROR: Insufficient token balance");
+        require(
+            balances[msg.sender][balances[msg.sender].length].value != 0, 
+            "ERROR: Insufficient token balance"
+            );
         performTransfer(msg.sender, _to, _amount);
         return success;
     }
 
     function approve(address _spender, uint _amount) override external returns (bool success) {
-        require(_spender != msg.sender, "ERROR: Unable to give your own address your allowance");
-        _allowanceOf[_spender][msg.sender] = _allowanceOf[_spender][msg.sender].add(_amount);
+        require(
+            _spender != msg.sender, 
+            "ERROR: Unable to give your own address your allowance"
+            );
+        performApprove(msg.sender, _spender, _amount);
         return success;
     }
 
-    // function transferFrom(address _from, address _to, uint _amount) override external returns (bool success) {
-    //     require(_allowanceOf[_from][msg.sender] >= _amount, "ERROR: Insufficient allowance amount");
-    //     _allowanceOf[_from][_to] = _allowanceOf[_from][_to].sub(_amount);
-    //     balances[_from] = balances[_from].sub(_amount);
-    //     balances[_to] = balances[_to].add(_amount);
-    //     return success;
-    // }
+    function transferFrom(address _from, address _to, uint _amount) override external returns (bool success) {
+        uint userAllowance = allowances[msg.sender][_from][allowances[msg.sender][_from].length].value;
+        require(
+            userAllowance >= _amount,
+            "ERROR: Insufficient allowance amount"
+            );
+        performApprove(_from, msg.sender, userAllowance.sub(_amount));
+        performTransfer(_from, _to, _amount);
+        return success;
+    }
 
     // ----------------------------------
     //           Extra API's
@@ -142,41 +150,41 @@ abstract contract FlareToken is ERC20 {
         uint newValue;
         uint newBlock;
     
-        newValue = balances[_from][(balances[_from].length)].value.sub(_amount);
+        newValue = balances[_from][balances[_from].length].value.sub(_amount);
         newBlock = block.number;
         balances[_from].push(Checkpoint(newValue, newBlock));
 
-        newValue = balances[_to][(balances[_to].length)].value.sub(_amount);
+        newValue = balances[_to][balances[_to].length].value.sub(_amount);
         newBlock = block.number; 
         balances[_to].push(Checkpoint(newValue, newBlock));
     
         emit Transfer(_from, _to, _amount);
     }
+
+    function performApprove(address _approver, address _spender, uint _amount) internal {
+        allowances[_spender][_approver].push(Checkpoint(_amount, block.number));
+        emit Approval(_approver, _spender, _amount);
+    }
     
+    function balanceOfAt(address _owner, uint _blockNumber) public view returns (uint) {
+        if (balances[msg.sender].length == 0) return 0;
 
-    // function balanceOfAt(address _owner, uint _blockNumber) public returns (uint) {
-    //     if (balances[msg.sender].length == 0) return 0;
-    //     // Shortcut for the actual value
-    //     if (_blockNumber >= balances[Checkpoint.length-1].fromBlock)
-    //         return balances[Checkpoint.length-1].value;
-    //     if (_blockNumber < balances[0].fromBlock) return 0;
+        // Shortcut for the actual value
+        if (_blockNumber >= balances[_owner][balances[_owner].length-1].fromBlock)
+            return balances[_owner][balances[_owner].length-1].value;
+        if (_blockNumber < balances[_owner][0].fromBlock) return 0;
 
-    //     // Binary search of the value in the array
-    //     uint min = 0;
-    //     uint max = balances.length-1;
-    //     while (max > min) {
-    //         uint mid = (max + min + 1)/ 2;
-    //         if (balances[mid].fromBlock<=_blockNumber) {
-    //             min = mid;
-    //         } else {
-    //             max = mid-1;
-    //         }
-    //     }
-    //     return balances[min].value;
-
-    // }
-
-    // function updateValueAtNow(uint _value) internal {
-
-    // }
+        // Binary search of the value in the array
+        uint min = 0;
+        uint max = balances[_owner].length-1;
+        while (max > min) {
+            uint mid = (max + min + 1)/ 2;
+            if (balances[_owner][mid].fromBlock<=_blockNumber) {
+                min = mid;
+            } else {
+                max = mid-1;
+            }
+        }
+        return balances[_owner][min].value;
+    }
 }
